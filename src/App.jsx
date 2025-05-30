84ecffe6864c5a34455f83032931073476ac83a7
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2025-05-30 18:49:44 +0530

    Refresh git index to fix casing issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-// import Home from './pages/home/home'
-import Home from './pages/home/Home'
+import Home from './pages/home/home'
+// import Home from './pages/home/Home'
 
 import { Route, Routes, Navigate} from 'react-router-dom'
 import SignUp from './pages/signup/SignUp'
